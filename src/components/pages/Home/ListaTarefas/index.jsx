df9b7b086c0fd54d4626cbdc0b009d4833dbc320
Sants16 <jvsantosrocha980@gmail.com>
2022-08-03 22:52:52 -0300

    comment_index

--- src/components/pages/Home/ListaTarefas/index.jsx
+++ src/components/pages/Home/ListaTarefas/index.jsx
@@ -3,7 +3,7 @@ import styles from './ListaTarefas.module.css'
 const ListaTarefas = () => {
     return (    
         <div className={styles.lista_tasks}>
-            <header className={styles.add_task_header}>
+            <header className={styles.add_task_header}> {/* transforma isso em um componente e pra saber como seguir essa parte do list-to-do tem o nosso projeto de base no github, basico-react */}
                 <input type="text" name="task" id="task" />
                 <button>Adicionar tarefa</button>
             </header>
